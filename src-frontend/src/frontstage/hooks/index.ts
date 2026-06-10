cbe3585a97050cdde0960efe304b973153cdc4dd
鱼在木 <yuzaimu@yuzaimudeMacBook-Pro.local>
2026-06-10 11:26:13 +0800

    fix: 移除 hooks/index.ts 中对不存在的 useFrontstageData 的导出

--- src-frontend/src/frontstage/hooks/index.ts
+++ src-frontend/src/frontstage/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useFrontstageData } from './useFrontstageData';
 export { default as useFrontstageEditor } from './useFrontstageEditor';
 export { default as useFrontstageGeneration } from './useFrontstageGeneration';
 export { default as useFrontstageWensi } from './useFrontstageWensi';
